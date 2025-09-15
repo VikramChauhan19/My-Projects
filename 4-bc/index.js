64102331b02113cff1d9aad021e1b9d5cb29d127
VikramChauhan19 <vikram9696chauhan@gmail.com>
2025-09-15 22:49:59 +0530

    static

--- 4-bc/index.js
+++ 4-bc/index.js
@@ -1,9 +1,10 @@
 const express= require("express")
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(express.static(path.join(__dirname, 'public'))); //all static filess in public
 app.set('view engine', 'ejs'); 
 
 app.get("/",function(req,res){
