e561b1a9428fef6c11e16c9aa52d5e4b9dce4430
VikramChauhan19 <vikram9696chauhan@gmail.com>
2025-09-23 00:48:14 +0530

    5-bc version 2

--- 5-Backend/app.js
+++ 5-Backend/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.json()); //Lets the server read JSON data sent from clients (like API requests).
+app.use(express.urlencoded({extended:true})); //Lets the server read form data (like from HTML forms).
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
-    console.log("hey")
+    res.render('index');
 })
 
 app.listen(3000);
